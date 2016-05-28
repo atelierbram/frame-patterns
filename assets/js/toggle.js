9dca773fd8f25d53e2e5b9091917bbc365717615
atelierbram <bramdeh@gmail.com>
2016-05-28 15:39:37 +0200

    added stamp border-image

--- assets/js/toggle.js
+++ assets/js/toggle.js
@@ -1,8 +1,8 @@
-$(function(){ 
+$(function(){
         "use strict";
   $('#btnToggle').click(function () {
     $('body').each(function(){
-      var classes = ['dv-01','dv-02','dv-03','dv-04','dv-05','dv-06','dv-07'];
+      var classes = ['dv-01','dv-02','dv-03','dv-04','dv-05','dv-06','dv-07','dv-08'];
 
       this.className = classes[($.inArray(this.className, classes)+1)%classes.length];
     });
